9a97e2dab06d6b582d71ffb6556a886b1fdf4bae
Kecman <kecman.dev@gmail.com>
2020-09-03 19:58:57 +0200

    initial

---
+++ src/components/layout.js
@@ -0,0 +1,74 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Header = styled.header`
+width:100%;
+border-bottom:1px solid black;
+display:flex;
+flex-direction:row;
+align-items: center;
+justify-content: space-between;
+`
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1>
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+      }}
+    >
+      <Header>
+        <div>{'</ kecman'}</div>
+        <div>Nemanja</div>
+      </Header>
+      
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
